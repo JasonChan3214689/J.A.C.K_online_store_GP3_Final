578c1b9d014506921df81a1945742e03994193c1
Cecilia <tongoiting3456@gmail.com>
2023-12-27 15:19:03 +0800

    hehe

--- src/TopBanner.js
+++ src/TopBanner.js
@@ -1,7 +1,30 @@
-export default function NavBar() {
+import ImageGallery from "react-image-gallery";
+import "./TopBanner.css";
+export default function TopBanner() {
+  const images = [
+    {
+      original: "../superMario_1600w",
+      thumbnail: "../superMario_1600w",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
   return (
     <div className="TopBannerContainer">
-      <img src="./TOPBANNER.png" alt="TopBanner"></img>
+      <ImageGallery items={images} />
     </div>
   );
+
+  // return (
+  //   <div className="TopBannerContainer">
+  //     <img src="./TOPBANNER.png" alt="TopBanner"></img>
+  //   </div>
+  // );
 }
