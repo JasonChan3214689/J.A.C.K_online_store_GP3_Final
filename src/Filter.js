39f349a1900701c10f9dd19ea11a0e53f52f2614
kaylie220 <148197573+kaylie220@users.noreply.github.com>
2024-01-02 10:58:10 +0800

    ilter function

--- src/Filter.js
+++ src/Filter.js
@@ -7,13 +7,12 @@ export default function Filter({ handleChange }) {
   const [open, setOpen] = useState(false);
   return (
     <div className="FilterCotainer">
-      <button>
-        <img src="/filter.png" alt="filter"></img>
       <button
         onClick={() => {
           setOpen(!open);
         }}
       >
+        <img src="/filter.png" alt="filter"></img>
         <img src="./filter.png" alt="filter"></img>
         Filter
       </button>
