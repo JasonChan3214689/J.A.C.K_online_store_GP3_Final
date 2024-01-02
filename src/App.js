f337d6cba25e338fea6705392ff29e5ce555e257
kaylie220 <148197573+kaylie220@users.noreply.github.com>
2024-01-02 11:27:56 +0800

    filter2

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const NintendoUrl =
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "c31c12f83bmshaa15045277e89acp1e97ffjsn2df9f8ba7cfa",
+    "X-RapidAPI-Key": "f4a32c8105msh6e6809ba17f74a9p150a68jsn46c52487869c",
     "X-RapidAPI-Host": "target1.p.rapidapi.com",
   },
 };
@@ -57,7 +57,7 @@ function App() {
     fetchAllData();
   }, []);
 
-  console.log(totalResults);
+  // console.log(totalResults);
 
   return (
     <Router>
--- src/Filter.js
+++ src/Filter.js
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import Brandfilter from "./Brandfilter";
-import Typefilter from "./Typefilter";
-import Pricefilter from "./Pricefilter";
 
 export default function Filter({ handleChange }) {
   const [open, setOpen] = useState(false);
@@ -12,17 +9,10 @@ export default function Filter({ handleChange }) {
           setOpen(!open);
         }}
       >
-        <img src="/filter.png" alt="filter"></img>
         <img src="./filter.png" alt="filter"></img>
         Filter
       </button>
-      {open && (
-        <div className="container">
-          <Brandfilter handleChange={handleChange} />
-          <Typefilter handleChange={handleChange} />
-          <Pricefilter handleChange={handleChange} />
-        </div>
-      )}
+      {open && <div className="container"></div>}
     </div>
   );
 }
