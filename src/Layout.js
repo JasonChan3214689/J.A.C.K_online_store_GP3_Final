75177f06a3301083754ec35b5822d4f394c1f457
kaylie220 <148197573+kaylie220@users.noreply.github.com>
2024-01-02 11:37:26 +0800

    filter3

--- src/Layout.js
+++ src/Layout.js
@@ -2,28 +2,25 @@ import React from "react";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
 import Filter from "./Filter";
-import LoginSidebarToggle from "./LoginSidebar";
+import Login from "./Login";
 import ShoppingCart from "./ShoppingCart";
 
 const Layout = ({ totalResults, onSearch }) => {
-	return (
-		<>
-			<div className="NavBar">
-				<NavBar />
-			</div>
-			<div className="Header">
-				<div className="SearchBarCotainer">
-					<SearchBar
-						totalResults={totalResults}
-						onSearch={onSearch}
-					/>
-				</div>
-				<Filter />
-				<LoginSidebarToggle />
-				<ShoppingCart />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="NavBar">
+        <NavBar />
+      </div>
+      <div className="Header">
+        <div className="SearchBarCotainer">
+          <SearchBar totalResults={totalResults} onSearch={onSearch} />
+        </div>
+        <Filter />
+        <Login />
+        <ShoppingCart />
+      </div>
+    </>
+  );
 };
 
 export default Layout;
